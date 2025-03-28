bb35814d039fd4db0685b9c885fa7788c082014e
Leonardo <leonardo.albuquerque.melo@gmail.com>
2025-03-28 10:25:29 -0300

    Class Seller adicionada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,21 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) throws Exception {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String data = "15/01/1994";
+        LocalDate birthDate = LocalDate.parse(data,df);
+
         Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", birthDate, 3000.00, obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
