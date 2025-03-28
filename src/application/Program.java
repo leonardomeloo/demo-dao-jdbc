1a829bee10603537f7e20828ede40ecc8ec6bbd8
Leonardo <leonardo.albuquerque.melo@gmail.com>
2025-03-28 11:03:33 -0300

    Adicionado SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,12 +12,16 @@ public class Program {
     public static void main(String[] args) throws Exception {
         DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String data = "15/01/1994";
-        LocalDate birthDate = LocalDate.parse(data,df);
+        LocalDate birthDate = LocalDate.parse(data, df);
 
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", birthDate, 3000.00, obj);
+
         System.out.println(obj);
+
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
+
         System.out.println(seller);
     }
 }
